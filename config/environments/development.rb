70bb48669e269bd041fbc33210e618c40749fb33
Sheila Leveille <sbleveille@gmail.com>
2020-06-16 21:06:43 +1000

    Created initial git repo for a chatroom to explore websockets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_view.embed_authenticity_token_in_remote_forms = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
